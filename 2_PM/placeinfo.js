a184970be6a19489384e7ca92993b646b5303901
Josh <jyee@college.harvard.edu>
2019-01-08 16:44:13 -0500

    2_PM solutions

--- 2_PM/placeinfo.js
+++ 2_PM/placeinfo.js
@@ -2,3 +2,27 @@
 / returns the following string, formatted with the appropriate values:
 
 'INPUTTED_PLACE is located at coordinates RETURNED_COORDINATES, at the address RETURNED_ADDRESS.' */
+
+const name = process.argv[2]
+const googleMapsClient = require('./google_api.js')
+
+googleMapsClient.findPlace({input: name, inputtype: 'textquery'}).asPromise()
+  .then((response) => {
+    let place_id
+    if (response.json.candidates != 0) {
+        place_id = response.json.candidates[0]['place_id']    
+    }
+    googleMapsClient.place({placeid: String(place_id)}).asPromise()
+    .then((response) => {
+        console.log(name + " is located at the coordinates " + response.json.result.geometry.location.lat + ", " + response.json.result.geometry.location.lng + " at the address " + response.json.result.formatted_address); 
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+ 
